659757d557b1cf028963abdf34c640c8f27aaf53
Elvis Leite <elvis7t@gmail.com>
2025-02-22 14:29:31 -0300

    remove arquivos de configuração e dados não utilizados

--- src/utils/build-route-path.js
+++
@@ -1,7 +0,0 @@
-export function buildRoutePath(path) {  
-    const routeParametersRegex = /:([a-zA-Z]+)/g;
-    const pathAithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)');
-
-    const pathRegex = new RegExp(`^${pathAithParams}(?<query>\\?(.*))?$`);
-    return pathRegex;
-}
--- src/utils/extract-query-params.js
+++
@@ -1,8 +0,0 @@
-export function extractQueryParams(queryString) {
-
-    return queryString.substr(1).split('&').reduce((queryParams, param) => {
-        const [key, value] = param.split('=');
-        queryParams[key] = value;
-        return queryParams;
-    }, {});
-}
